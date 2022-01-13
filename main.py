a1853d617a1f21e505f31c6c5f257af1f5d53773
Georgejcmp <george.moreno@yahoo.com.br>
2022-01-13 15:10:50 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Calculo de passagem
+
+passagem = 1500
+custo_por_dia = 350
+dias = 14
+dolar = 5.75
+custo_total = passagem * 2 + custo_por_dia * dias
+
+print("O custo da sua viagem será: R$", custo_total)
+print("O custo da sua viagem em dolar será: U$", custo_total / dolar)
