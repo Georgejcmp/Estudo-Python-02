bbdd599448ed0f806c0800f233942bba6d00712a
Georgejcmp <george.moreno@yahoo.com.br>
2022-01-13 15:15:35 -0300

    Alteração do arquivo README

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ dias = 14
 dolar = 5.75
 custo_total = passagem * 2 + custo_por_dia * dias
 
+
 print("O custo da sua viagem será: R$", custo_total)
 print("O custo da sua viagem em dolar será: U$", custo_total / dolar)
